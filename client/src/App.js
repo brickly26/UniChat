566bb0a1d20caeb346ce37c22aee04b9ebbc23fe
brickly26 <sabburak26@gmail.com>
2022-08-02 13:54:13 -0400

    created login form that handles for submit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelListContainer, ChannelContainer, Auth } from './components';
+
+import './App.css';
+
+const apiKey = 'sw7uvfepbjy9';
+
+const client = StreamChat.getInstance(apiKey);
+
+const authToken = false;
 
 const App = () => {
+
+  if(!authToken) return <Auth />
+  
   return (
-    <div>
-      <h1>Medical pager</h1>
+    <div className='app__wrapper'>
+      <Chat client={client} theme="team light">
+        <ChannelListContainer
+          
+        />
+        <ChannelContainer
+
+        />
+      </Chat>
     </div>
   )
 }
